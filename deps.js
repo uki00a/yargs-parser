22413d6322946ae09f8294b961078604451a228b
uki00a <uki00a@gmail.com>
2020-03-21 20:55:32 +0900

    update: bump deno-yargs-compat to v0.0.4

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
 import decamelize from "https://dev.jspm.io/decamelize@1.2.0"
 import camelCase from "https://dev.jspm.io/camelcase@5.3.1"
-import * as compat from "https://denopkg.com/uki00a/deno-yargs-compat@v0.0.3/mod.ts"
+import * as compat from "https://denopkg.com/uki00a/deno-yargs-compat@v0.0.4/mod.ts"
 
 export { decamelize, camelCase, compat }
